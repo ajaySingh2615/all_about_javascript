8144f16443c90e8843dcc670b3f205614e65fb05
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-04-01 22:19:30 +0530

    "Async Intro"

---
+++ 08_Async/intro.js
@@ -0,0 +1,14 @@
+/*
+    JavaScript is Synchronous
+    JavaScript is single threaded
+
+    Execution Context
+        1. Execute one line of code at a time
+        2. Each operation waits for the last one to compelete before executing
+        3. Call Stack
+        4. Memory Heap
+
+    # Blocking code vs Non Blocking code
+        Blocking Code - Block the flow of program - Read file sync
+        Non Blocking code - Does not block execution - Read file Async
+*/
